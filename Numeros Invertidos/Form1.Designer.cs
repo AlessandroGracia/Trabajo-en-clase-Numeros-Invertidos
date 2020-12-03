89069385c0430c5b29fc50cd639bdef497ad5aec
ALESSANDRO EMANUELLE GRACIA ALBAN <alessandro.gracia@pucese.edu.ec>
2020-12-02 20:21:48 -0500

    Agregar archivos de proyecto.

---
+++ Numeros Invertidos/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Numeros_Invertidos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bntCaculcuar = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.txtInvertido = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(350, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numeros Invertidos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese un Numero";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Numero Invertido";
+            // 
+            // bntCaculcuar
+            // 
+            this.bntCaculcuar.Location = new System.Drawing.Point(527, 118);
+            this.bntCaculcuar.Name = "bntCaculcuar";
+            this.bntCaculcuar.Size = new System.Drawing.Size(107, 34);
+            this.bntCaculcuar.TabIndex = 3;
+            this.bntCaculcuar.Text = "CALCULAR";
+            this.bntCaculcuar.UseVisualStyleBackColor = true;
+            this.bntCaculcuar.Click += new System.EventHandler(this.bntCaculcuar_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(222, 82);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 4;
+            // 
+            // txtInvertido
+            // 
+            this.txtInvertido.Location = new System.Drawing.Point(222, 156);
+            this.txtInvertido.Name = "txtInvertido";
+            this.txtInvertido.Size = new System.Drawing.Size(100, 20);
+            this.txtInvertido.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 272);
+            this.Controls.Add(this.txtInvertido);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.bntCaculcuar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button bntCaculcuar;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.TextBox txtInvertido;
+    }
+}
+
---
+++ Numeros Invertidos/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numeros_Invertidos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bntCaculcuar_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(txtNumero.Text);
+            int res = procesarNum(num);
+            txtInvertido.Text = res.ToString();
+        }
+        int procesarNum(int num)
+        {
+            int invertido = 0;
+            while (num >0)
+            {
+                invertido = invertido * 10 + num % 10;
+                num = num / 10;
+            }
+            return invertido;
+            
+            
+        }
+
+    }
+}
